241dee0c9d901b3fb41d5016eadfb343d0e8aefb
kevin_xie <xie.kevin.j@gmail.com>
2020-08-31 01:07:09 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -6,32 +6,78 @@ const baseUrl = "http://localhost:3000/pups/";
 const getPups = () => {
   fetch(baseUrl)
     .then((resp) => resp.json())
-    .then((pups) => renderP(pups));
+    .then((pups) => renderPups(pups));
 };
-
-const renderP = (pups) => {
+const renderAPup = (pup) => {
   const dogBox = document.querySelector("#dog-summary-container");
   const dogBar = document.querySelector("#dog-bar");
+  const span = document.createElement("span");
 
-  for (const pup of pups) {
+  span.innerText = pup.name;
+
+  const dogSwitch = () => {
     const div = document.createElement("div");
-    const span = document.createElement("span");
-
-    span.innerText = pup.name;
+    div.setAttribute("data-id", pup.id);
 
     div.classList.add("dog-info");
-    div.innerHTML = `<img src=${pup.image}> <h2>${pup.name}</h2>`;
-    dogBox.append(div);
+    div.innerHTML = `<img src=${pup.image} class="image"> <h2>${pup.name}</h2>`;
 
-    const dogSwitch = (e) => {
-      const button = e.target;
-      // putDoge()
+    const button = document.createElement("button");
+    if (pup.isGoodDog) {
+      button.innerText = "bad puppy";
+    } else if (!pup.isGoodDog) {
+      button.innerText = "Is a good puppiee";
+    }
+
+    const goodBadSwitcheroo = () => {
+      //   console.log(pup.isGoodDog);
+      putDoge(pup.id, !pup.isGoodDog);
+      button.removeEventListener("click", goodBadSwitcheroo);
     };
 
-    span.addEventListener("click", dogSwitch);
-    // span.innerHTML =
-    dogBar.append(span);
+    button.addEventListener("click", goodBadSwitcheroo);
+    dogBox.innerHTML = "";
+    div.append(button);
+    dogBox.append(div);
+  };
+
+  span.addEventListener("click", dogSwitch);
+  dogBar.append(span);
+};
+const renderPups = (pups) => {
+  for (const pup of pups) {
+    renderAPup(pup);
   }
 };
 
-// const putDoge = () =>
+const putDoge = (id, isGoodDog) => {
+  let formData = {
+    isGoodDog: isGoodDog,
+  };
+
+  let configObj = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  fetch(`${baseUrl}${id}`, configObj)
+    .then((resp) => resp.json())
+    .then((pup) => {
+      const button = document.querySelector(`[data-id="${pup.id}"] button`);
+      if (pup.isGoodDog) {
+        button.innerText = "bad puppy";
+      } else if (!pup.isGoodDog) {
+        button.innerText = "Is a good puppiee";
+      }
+      const goodBadSwitcheroo = () => {
+        //   console.log(pup.isGoodDog);
+        putDoge(pup.id, !pup.isGoodDog);
+        button.removeEventListener("click", goodBadSwitcheroo);
+      };
+
+      button.addEventListener("click", goodBadSwitcheroo);
+    });
+};
